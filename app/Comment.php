a8ff5fd951778cf57fff19a03e9fe90a38235a5f
Arnaud BACH <arnaud.bach1987@gmail.com>
2017-08-23 16:47:49 +0200

    [UPDATE] - ADD FRONT PAGE

--- app/Comment.php
+++ app/Comment.php
@@ -4,14 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Presenters\DatePresenter;
+
 class Comment extends Model
 {
+    use DatePresenter;
     //
     public function posts()
     {
         return $this->belongsTo('App\Post');
     }
-    public function users()
+    public function user()
     {
         return $this->belongsTo('App\User');
     }

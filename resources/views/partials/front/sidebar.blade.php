f09f45331c1fd256592aba1470b5cc6d76e145f7
brakni amine <amine.brakni@gmail.com>
2017-08-25 13:23:46 +0200

    [UPDATE] - Height timeline twitter

--- resources/views/partials/front/sidebar.blade.php
+++ resources/views/partials/front/sidebar.blade.php
@@ -1,6 +1,6 @@
 @section('sidebar')
     <!-- Side Widget Well -->
     <div class="well">
-        <a class="twitter-timeline" data-lang="fr" data-height="450" data-theme="light" data-link-color="#2B7BB9 " href="https://twitter.com/lemdev">Tweets by lemdev</a>
+        <a class="twitter-timeline" data-lang="fr" data-height="550" data-theme="light" data-link-color="#2B7BB9 " href="https://twitter.com/lemdev">Tweets by lemdev</a>
     </div>
 @show
